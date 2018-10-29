db60c127ddc1a8c53b78f4ba455ab1a446dbeecf
user <870649807@qq.com>
2018-10-29 08:11:21 +0800

    Configurator等的初始化

---
+++ fast-core/src/main/java/com/stnebula/fast_core/app/Fast.java
@@ -0,0 +1,11 @@
+package com.stnebula.fast_core.app;
+
+/**
+ * @project FestECImitate
+ * @package com.stnebula.fast_core.app
+ * @anthor Lenovo ST
+ * @time 2018/10/28 14:12
+ * @class TODO
+ */
+public final class Fast {
+}
---
+++ fast-ec/src/main/java/com/stnebula/fastec/App.java
@@ -0,0 +1,11 @@
+package com.stnebula.fastec;
+
+/**
+ * @project FestECImitate
+ * @package com.stnebula.fastec
+ * @anthor Lenovo ST
+ * @time 2018/10/28 14:44
+ * @class TODO
+ */
+public class App {
+}
